79a91e52e8701370c220aa373a6b74347ec368bd
Paolo <u20221f613@upc.edu.pe>
2025-10-03 17:57:11 -0500

    chore: add ConnectingScreen to be realistic

---
+++ app/src/main/java/upc/edu/pe/levelupjourney/presentation/screen/join/ConnectingScreen.kt
@@ -0,0 +1,47 @@
+package upc.edu.pe.levelupjourney.presentation.screen.join
+
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import upc.edu.pe.levelupjourney.R
+
+@Composable
+fun ConnectingScreen(
+    onFinished: () -> Unit
+) {
+    // animación de rotación
+    val rotation = remember { Animatable(0f) }
+
+    LaunchedEffect(Unit) {
+        rotation.animateTo(
+            targetValue = 360f,
+            animationSpec = tween(durationMillis = 2000, easing = LinearEasing)
+        )
+        delay(200) // pequeño delay antes de continuar
+        onFinished()
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Connecting")
+        Spacer(Modifier.height(24.dp))
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .size(80.dp)
+                .rotate(rotation.value)
+        )
+    }
+}
