e5e43e5fdf2aa4f15a80904e8169bdb7dea8ac15
Paolo <u20221f613@upc.edu.pe>
2025-10-03 16:31:13 -0500

    chore: create data class post

---
+++ app/src/main/java/upc/edu/pe/levelupjourney/domain/model/Post.kt
@@ -0,0 +1,23 @@
+package upc.edu.pe.levelupjourney.domain.model
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+class Post(
+    val id: Int,
+    val user: String,
+    val description: String,
+    comments: Int = 0,
+    likes: Int = 0
+) {
+    var liked by mutableStateOf(false)
+    var likes by mutableStateOf(likes)
+    var comments by mutableStateOf(comments)
+}
+
+data class Comment(
+    val id: Int,
+    val user: String,
+    val text: String
+)
