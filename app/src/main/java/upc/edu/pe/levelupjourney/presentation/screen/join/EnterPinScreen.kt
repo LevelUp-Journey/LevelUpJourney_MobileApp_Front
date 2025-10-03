52f198365410a8f979860f7eefd82420eada9bc5
Paolo <u20221f613@upc.edu.pe>
2025-10-03 17:57:25 -0500

    chore: add EnterPinScreen

---
+++ app/src/main/java/upc/edu/pe/levelupjourney/presentation/screen/join/EnterPinScreen.kt
@@ -0,0 +1,72 @@
+package upc.edu.pe.levelupjourney.presentation.screen.join
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun EnterPinScreen(
+    onBackToHome: () -> Unit,
+    onEnterGame: () -> Unit,
+    onScanCode: () -> Unit
+) {
+    var pin by remember { mutableStateOf("") }
+
+    Scaffold(
+        topBar = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = onBackToHome) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "Close"
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            OutlinedTextField(
+                value = pin,
+                onValueChange = { pin = it },
+                label = { Text("Enter PIN") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Button(onClick = onEnterGame, enabled = pin.isNotBlank()) {
+                Text("Enter Game")
+            }
+
+            Spacer(modifier = Modifier.height(48.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(onClick = { /* estamos en esta vista */ }) {
+                    Text("Enter Pin")
+                }
+                Button(onClick = onScanCode) {
+                    Text("Scan Code")
+                }
+            }
+        }
+    }
+}
